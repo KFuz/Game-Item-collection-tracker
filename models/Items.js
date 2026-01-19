86e64280894188f3874df43313a810ce84ebc250
KFuz <k.fuzaia@gmail.com>
2026-01-19 19:44:56 +0300

    added items model

---
+++ models/Items.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Game = require("./Games");
+
+const itemSchema = new mongoose.Schema({
+  itemName: {
+    type: String,
+    required: true,
+  },
+  itemRarity:{
+    type: String,
+    required: true,
+
+  },
+  
+  game: { type: mongoose.Schema.Types.ObjectId, ref: 'Game' },
+
+});
+
+const Game = mongoose.model("Game", itemSchema);
+
+module.exports = Game;
