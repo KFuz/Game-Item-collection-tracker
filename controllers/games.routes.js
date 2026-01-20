b32dbaeb429dae9b1aa8e3dad2b67db24e3ea5cd
KFuz <k.fuzaia@gmail.com>
2026-01-20 18:55:00 +0300

    create game route created

--- controllers/games.routes.js
+++ controllers/games.routes.js
@@ -1,5 +1,5 @@
 const { route } = require("./auth");
-
+const Game = require("../models/Games");
 const router = require("express").Router()
 
 
@@ -13,3 +13,11 @@ module.exports = router;
 // add delete game form/page it should also delete items from 
 // items list as well
 //add games details page
+
+// Show create games
+router.get("/create", async (req, res) => {
+  const games = await Game.find();
+  res.render("../views/Games/creategame.ejs", { games });
+});
+
+ 
