3b4ea575576d0db831d2b843f257a1340522e704
KFuz <k.fuzaia@gmail.com>
2026-01-21 19:02:06 +0300

    added update games + homepage must be logged in for links

--- controllers/games.routes.js
+++ controllers/games.routes.js
@@ -25,15 +25,27 @@ router.post('/createGame', async(req,res)=>{
     const createdGame = await Game.create(req.body)
     res.redirect('/games/creategame')
 })
-
+//list all 
 router.get("/mygames", async (req, res) => {
   const allGames = await Game.find();
   res.render("games/mygames", {allGames:allGames});
   console.log(allGames)
 });
-
+// delete
 router.post("/delete/:id",async(req,res)=>{
   await Game.findByIdAndDelete(req.params.id)
+    res.redirect('/Games/mygames')
+})
+// update
+
+router.get('/update/:id', async(req,res)=>{
+ const foundGame = await Game.findById(req.params.id)
+res.render('games/updategame.ejs',{foundGame: foundGame})
+ })
+
+router.post('/update/:id', async (req,res)=>{
+ 
+ await Game.findByIdAndUpdate(req.params.id, req.body)
     res.redirect('/Games/mygames')
 })
 
