93053f5457c8efa8973853a9430e94b45b8dbabc
KFuz <k.fuzaia@gmail.com>
2026-01-26 18:07:33 +0300

    item routes created

--- controllers/items.routes.js
+++ controllers/items.routes.js
@@ -9,6 +9,41 @@ router.get('/',(req,res)=>{
     res.render('items/myitems.ejs')
 })
 
+//item create
+
+router.get("/createitem", (req, res) => {
+  res.render('items/createitem.ejs');
+});
+
+
+router.post('/createitem', async(req,res)=>{
+    req.body.creator = req.session.user_id
+    const createdGame = await Game.create(req.body)
+    res.redirect('/items/createitem')
+})
+//list all 
+router.get("/myitems", async (req, res) => {
+  const allItems = await Game.find();
+  res.render("items/myitems", {allItems:allItems});
+  console.log(allItems)
+});
+// delete
+router.post("/delete/:id",async(req,res)=>{
+  await Item.findByIdAndDelete(req.params.id)
+    res.redirect('/items/myitems')
+})
+// update
+
+router.get('/update/:id', async(req,res)=>{
+ const foundGame = await Game.findById(req.params.id)
+res.render('items/updateitem.ejs',{foundGame: foundGame})
+ })
+
+router.post('/update/:id', async (req,res)=>{
+ 
+ await Item.findByIdAndUpdate(req.params.id, req.body)
+    res.redirect('/items/myitems')
+})
 
 
 
